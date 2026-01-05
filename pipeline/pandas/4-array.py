937e597a5ee0e2061805bdbd6e5a401e09d75486
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-05 12:46:41 +0000

    a

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -6,7 +6,8 @@
 
 def array(df):
     """
-    Selects the last 10 rows of the 'High' and 'Close' columns from the DataFrame
+    Selects the last 10 rows of the 'High'
+    and 'Close' columns from the DataFrame
     and converts them into a numpy array.
     """
     # Select the last 10 rows of 'High' and 'Close' columns
