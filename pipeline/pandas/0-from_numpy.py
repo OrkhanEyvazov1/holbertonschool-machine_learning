748894dbe898945f79d8e1d9f218d2f397ad3106
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-05 11:56:25 +0000

    a

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -4,9 +4,11 @@
 
 import pandas as pd
 
+
 def from_numpy(array):
     """
-    alphabetical and capitalized column names for a DataFrame created from a NumPy array.
+    alphabetical and capitalized column namesi 
+    for a DataFrame created from a NumPy array.
     """
     num_columns = array.shape[1]
     columns = [chr(i) for i in range(65, 65 + num_columns)]
