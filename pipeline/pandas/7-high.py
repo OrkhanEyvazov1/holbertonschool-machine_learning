d70c293c08680b1993b8725e8a45bea90d567e65
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-06 09:05:22 +0000

    a

--- pipeline/pandas/7-high.py
+++ pipeline/pandas/7-high.py
@@ -2,14 +2,12 @@
 
 
 """
-New code sorts the DataFrame by the High price in descending order
+Tester
 """
 
 
-from_file = __import__('2-from_file').from_file
-
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
-df = df.sort_values(by='High', ascending=False)
-
-print(df.head())
+def high(df):
+    '''
+    doc hereee
+    '''
+    return df.sort_values(by='High', ascending=False)
