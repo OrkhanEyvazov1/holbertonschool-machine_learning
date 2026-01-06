8154b2f9af5ed3b54ac602d916be82ccd67d97a7
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-06 09:19:05 +0000

    fill

---
+++ pipeline/pandas/9-fill.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+''' doc here '''
+
+
+def fill(df):
+    ''' doc here '''
+    df=df.drop(columns=['Weighted_Price'])
+    df['Close'] = df['Close'].ffill()
+
+    df['High'] = df['High'].fillna(df['Close'])
+    df['Low'] = df['Low'].fillna(df['Close'])
+    df['Open'] = df['Open'].fillna(df['Close'])
+
+    df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
+    df['Volume_(Currency)'] = df['Volume_(Currency)'].fillna(0)
+
+    return df
