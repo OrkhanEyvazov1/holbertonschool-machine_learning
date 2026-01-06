6fa74c5540589d5a49b6ec30b772f2251c954e65
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-06 09:55:26 +0000

    a

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+
+''' doc here'''
+
+
+import pandas as pd
+
+
+index = __import__('10-index').index
+
+
+def hierarchy(df1, df2):
+    """
+    Concatenates two dataframes with a hierarchical index.
+
+    Args:
+        df1: First pandas DataFrame (coinbase).
+        df2: Second pandas DataFrame (bitstamp).
+
+    Returns:
+        The concatenated pandas DataFrame.
+    """
+    r = range(1417411980, 1417417980 + 60, 60)
+    df1_filtered = df1.loc[df1.index.isin(r)]
+    df2_filtered = df2.loc[df2.index.isin(r)]
+
+    df = pd.concat([df2_filtered, df1_filtered], keys=['bitstamp', 'coinbase'])
+    
+    df = df.swaplevel(0, 1).sort_index()
+
+    return df
