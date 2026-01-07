32751781721cc00f7f3c7906f9c083d0d16af1c1
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-07 15:03:04 +0400

    a

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -11,5 +11,6 @@ import matplotlib.pyplot as plt
 def line():
     '''doc here'''
     y = np.arange(0, 11) ** 3
-    plt.figure(figsize=(6.4, 4.8))
+    plt.figure(figsize = (6.4, 4.8))
     plt.plot(y, 'r-')
+    plt.xlim(0, 10)
