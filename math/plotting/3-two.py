ef67cb5e2cd69863832267420a4e43b86ebb3d36
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-07 15:23:51 +0400

    a

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -19,6 +19,7 @@ def two():
     plt.xlabel('Time (years)')
     plt.ylabel('Fraction Remaining')
     plt.title('Exponential Decay of Radioactive Elements')
+    plt.legend()
     plt.xlim(0, 20000)
     plt.ylim(0, 1)
     plt.show()
