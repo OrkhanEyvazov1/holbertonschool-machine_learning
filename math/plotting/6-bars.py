bce56dfcd562ae805981feef257bb16d46982019
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-07 16:31:28 +0400

    a

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,37 +1,28 @@
 #!/usr/bin/env python3
-""" plot a stacked bar chart for the amount and types of fruit people have """
 
+'''bar chart example'''
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-np.random.seed(5)
-fruit = np.random.randint(0, 20, (4, 3))
-
-people = ['Farrah', 'Fred', 'Felicia']
-fruit_names = {
-    'apples': 'red',
-    'bananas': 'yellow',
-    'oranges': '#ff8000',
-    'peaches': '#ffe5b4'
-}
-
-i = 0
-for name, color in sorted(fruit_names.items()):
-    bottom = 0
-    for j in range(i):
-        bottom += fruit[j]
-    plt.bar(
-        np.arange(len(people)),
-        fruit[i],
-        width=0.5,
-        bottom=bottom,
-        color=color,
-        label=name)
-    i += 1
-plt.xticks(np.arange(len(people)), people)
-plt.yticks(np.arange(0, 81, 10))
-plt.ylabel('Quantity of Fruit')
-plt.title("Number of Fruit per Person")
-plt.legend()
-plt.show()
+def bars():
+    '''Bar chart example'''
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
+    people = ['Farrah', 'Fred', 'Felicia']
+    fruit_types = ['apples', 'bananas', 'oranges', 'peaches']
+    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+    x = np.arange(len(people))
+    bottom = np.zeros(len(people))
+    for counts, label, color in zip(fruit, fruit_types, colors):
+        plt.bar(x, counts, bottom=bottom, width=0.5, color=color, label=label.capitalize())
+        bottom += counts
+    plt.xticks(x, people)
+    plt.yticks(np.arange(0, 81, 10))
+    plt.ylim(0, 80)
+    plt.ylabel('Quantity of Fruit')
+    plt.title('Number of Fruit per Person')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
