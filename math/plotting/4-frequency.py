aec2847f030e2f986f7bf7aa0aad3950ed498d62
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-07 15:47:07 +0400

    a

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def frequency():
     '''Frequency distribution'''
     np.random.seed(5)
@@ -15,5 +16,5 @@ def frequency():
     plt.title('Project A')
     plt.xlim(0, 100)
     plt.xticks(np.arange(0, 101, 10))
-    plt.ylim(0,30)
+    plt.ylim(0, 30)
     plt.show()
