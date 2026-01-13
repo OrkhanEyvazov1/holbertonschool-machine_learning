6eb636ee0a125eafe55be83bf7b70a22307c4491
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-13 12:01:34 +0000

    poly of integ

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,16 +4,27 @@
 '''poly of integral'''
 
 
-def poly_integral(poly):
-    """Calculate the integral of a polynomial."""
+def poly_integral(poly, C=0):
+    '''poly of integral'''
+    if type(C) is not int:
+        return None
+
     if not isinstance(poly, list) or len(poly) == 0:
         return None
+
     for coef in poly:
-        if not isinstance(coef, (int, float)):
+        if not isinstance(coef, (int, float)) or isinstance(coef, bool):
             return None
-    integral = [0.0]
+
+    integral = [C]
     for i in range(len(poly)):
-        new_coeff = poly[i] / (i + 1)
-        integral.append(new_coeff)
+        val = poly[i] / (i + 1)
+        if val % 1 == 0:
+            val = int(val)
+
+        integral.append(val)
+
+    while len(integral) > 1 and integral[-1] == 0:
+        integral.pop()
 
     return integral
