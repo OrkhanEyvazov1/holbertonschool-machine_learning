22a534863fb2bde1e0cbd1752d69a511af62e5f6
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-13 11:14:51 +0000

    derv

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+''' poly of derv '''
+
+
+def poly_derivative(poly):
+    '''poly of dev doc'''
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    
+    for coef in poly:
+        if not isinstance(coef, (int, float)):
+            return None
+
+    if len(poly) == 1:
+        return [0]
+    
+    derivative = []
+    for power in range(1, len(poly)):
+        new_coef = power * poly[power]
+        derivative.append(new_coef)
+
+    return derivative
