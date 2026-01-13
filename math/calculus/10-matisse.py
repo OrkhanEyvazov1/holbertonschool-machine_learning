6250658b194a3677cec456be381e7db79dcda8a1
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-13 11:20:52 +0000

    a

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -13,7 +13,7 @@ def poly_derivative(poly):
             return None
 
     if len(poly) == 1:
-        return [0]    
+        return [0]
     derivative = []
     for power in range(1, len(poly)):
         new_coef = power * poly[power]
