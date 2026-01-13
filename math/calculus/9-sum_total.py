1975d6f8eaf137c8a655b580015593af068b8263
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-13 10:48:33 +0000

    sum of isq

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python3
+"""function that sum powers based on Faulhaber's Formula"""
 
-
-'''doc heree'''
 
 def summation_i_squared(n):
-    '''sum of i sq'''
-    # 1. Check if n is a boolean (some graders fail if True/False are allowed)
-    if isinstance(n, bool):
+    """m(m+1)(2m+1) / 6"""
+
+    if type(n) is not int:
         return None
-
-    # 2. Check if n is a number (int or float)
-    if not isinstance(n, (int, float)):
+    elif n < 1:
         return None
-
-    # 3. Check if it's a negative number or has a decimal remainder
-    if n < 0 or n % 1 != 0:
-        return None
-
-    # Cast to int to ensure the formula works with large numbers correctly
-    n = int(n)
-
-    # The closed-form formula: n(n+1)(2n+1) / 6
-    return (n * (n + 1) * (2 * n + 1)) // 6
+    else:
+        return int((n * (1 + n)) * (2 * n + 1) / 6)
