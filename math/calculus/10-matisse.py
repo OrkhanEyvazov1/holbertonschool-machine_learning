a40d44d7ab5b26d0191d9d6fb98a68e1cb2fc11b
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-13 11:19:51 +0000

    m

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -8,14 +8,12 @@ def poly_derivative(poly):
     '''poly of dev doc'''
     if not isinstance(poly, list) or len(poly) == 0:
         return None
-    
     for coef in poly:
         if not isinstance(coef, (int, float)):
             return None
 
     if len(poly) == 1:
-        return [0]
-    
+        return [0]    
     derivative = []
     for power in range(1, len(poly)):
         new_coef = power * poly[power]
