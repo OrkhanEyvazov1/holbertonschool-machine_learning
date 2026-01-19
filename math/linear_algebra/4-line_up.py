54cc9d529ec61216e82e5e732ed84c6fbb4a9013
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-19 10:51:24 +0000

    a

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-
-'''doc here '''
+'''doc her '''
 
 
 def add_arrays(arr1, arr2):
     ''' adds two arrays element-wise '''
     if len(arr1) != len(arr2):
-        raise None
-    result = []
+        return None
+    new_list = []
     for i in range(len(arr1)):
-        result.append(arr1[i] + arr2[i])
-    return result
+        new_list.append(arr1[i] + arr2[i])
+    return new_list
