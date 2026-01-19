01f1e6a09fb6aa8b79b7dba54fe50e012cc52203
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-19 10:11:42 +0000

    a

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -8,13 +8,9 @@ def matrix_shape(matrix):
     ''' shape of matrix '''
     shape = []
     current_layer = matrix
-    
     while isinstance(current_layer, list):
         shape.append(len(current_layer))
-        
         if len(current_layer) == 0:
             break
-            
         current_layer = current_layer[0]
-        
     return shape
