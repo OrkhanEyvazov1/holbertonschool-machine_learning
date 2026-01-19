39156698866afc11fa38cd19d82a41b1288c3bca
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-19 09:44:48 +0000

    shape

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+'''return shape of matrix'''
+
+
+def matrix_shape(matrix):
+    ''' fn here '''
+    lst=[]
+    lst.append(len(matrix[0]))
+    lst.append(len(matrix))
+    return lst
