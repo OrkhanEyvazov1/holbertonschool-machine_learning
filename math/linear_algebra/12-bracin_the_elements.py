dbc0a767c24d718ef9a58dc1121df674c1a5163b
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-21 10:20:08 +0000

    a

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' doc here '''
+
+
+def np_elementwise(mat1, mat2):
+    ''' elementwise operations numpy '''
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
