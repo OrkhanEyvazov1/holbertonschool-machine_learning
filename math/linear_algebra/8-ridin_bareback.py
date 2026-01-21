0dcfc5be175354e540981ac774548953bc03e32f
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-21 10:03:42 +0000

    a

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+''' matrix mult '''
+
+
+def mat_mul(mat1, mat2):
+    ''' matrix multiplication '''
+    if len(mat1) != len(mat2[0]):
+        return None
+    else:
+        mat2_t = list(zip(*mat2))
+        return [
+            [sum(a * b for a, b in zip(row_a, col_b)) for col_b in mat2_t]
+            for row_a in mat1
+        ]
