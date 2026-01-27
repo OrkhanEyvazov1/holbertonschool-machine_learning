c13040f3ce062cd9c3b1aa474c0b74ba119966e4
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-27 10:36:47 +0000

    determinant

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -3,22 +3,30 @@
 
 
 def determinant(matrix):
-    """Calculates the determinant of a 2x2 or 3x3 matrix."""
-    if not isinstance(matrix, list) or not all(
-            isinstance(row, list) for row in matrix):
+    """Calculates the determinant of a matrix."""
+    if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
-    if len(matrix) != len(matrix[0]):
-        raise TypeError("matrix must be a square matrix")
-    if len(matrix) == 1:
+
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix == [[]]:
         return 1
-    if len(matrix) == 2:
+
+    n = len(matrix)
+    for row in matrix:
+        if len(row) != n:
+            raise ValueError("matrix must be a square matrix")
+
+    if n == 1:
+        return matrix[0][0]
+
+    if n == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    elif len(matrix) == 3:
-        return (matrix[0][0] * (matrix[1][1] * matrix[2][2] -
-                                matrix[1][2] * matrix[2][1]) -
-                matrix[0][1] * (matrix[1][0] * matrix[2][2] -
-                                matrix[1][2] * matrix[2][0]) +
-                matrix[0][2] * (matrix[1][0] * matrix[2][1] -
-                                matrix[1][1] * matrix[2][0]))
-    else:
-        return None  # None for higher order matrices
+
+    det = 0
+    for c in range(n):
+        sub_matrix = [row[:c] + row[c + 1:] for row in matrix[1:]]
+        det += ((-1) ** c) * matrix[0][c] * determinant(sub_matrix)
+
+    return det
