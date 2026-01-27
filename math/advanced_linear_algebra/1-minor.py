5416da1a8eb41fb87185f2857a12ff2a2eec4c2c
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-01-27 11:25:33 +0000

    multi no

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -26,6 +26,33 @@ def determinant(matrix):
     return multi_determinant(matrix)
 
 
+def multi_determinant(matrix):
+    """
+    function that computes the determinant of a given matrix of
+    +3D
+    Args:
+        matrix: list of lists whose determinant should be calculated
+    Returns: Determinant of matrix
+    """
+    mat_l = len(matrix)
+    if mat_l == 2 and len(matrix[0]) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+
+    deter = 0
+    cols = list(range(len(matrix)))
+    for c in cols:
+        mat_cp = [r[:] for r in matrix]
+        mat_cp = mat_cp[1:]
+        rows = range(len(mat_cp))
+
+        for r in rows:
+            mat_cp[r] = mat_cp[r][0:c] + mat_cp[r][c + 1:]
+        sign = (-1) ** (c % 2)
+        sub_det = multi_determinant(mat_cp)
+        deter += sign * matrix[0][c] * sub_det
+    return deter
+
+
 def minor_val(matrix, idx_r, idx_c):
     """
     function that computes minor in each idx position of the given matrix
