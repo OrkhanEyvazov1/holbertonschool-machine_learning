ffe500e8393e836421dd9999a242ff5b39aa7192
OrkhanEyvazov <orkhane50616@sabah.edu.az>
2026-02-02 09:23:19 +0000

    pdf

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -18,3 +18,20 @@ class Poisson:
                 raise ValueError("data must contain multiple values")
 
             self.lambtha = float(sum(data) / len(data))
+
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of successes
+        Args:
+            k: number of successes
+        Returns: PMF value for k
+        """
+        k = int(k)
+        factorial_k = 1
+        if k < 0:
+            return 0
+        for i in range(1, k + 1):
+            factorial_k *= i
+        pmf = Poisson.e ** -self.lambtha * self.lambtha ** k / factorial_k
+        return pmf
