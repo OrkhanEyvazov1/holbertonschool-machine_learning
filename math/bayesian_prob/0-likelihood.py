89cd814236bb72313e0567813ac81ed42ff60c69
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-02-04 21:05:08 +0000

    minor checking

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -4,28 +4,31 @@ Docstring for math tasks
 '''
 
 
-def likelihood(x, n, p):
+def likelihood(x, n, P):
     """
     Calculates the likelihood of obtaining this data given various
     probabilities of developing severe side effects
     Args:
         x: number of patients that develop severe side effects
         n: total number of patients observed
-        p: numpy.ndarray of various probabilities
-    Returns: numpy.ndarray containing the likelihood of obtaining the data
-             for each probability in p
+        P: 1D numpy.ndarray containing the various hypothetical probabilities
+           of developing severe side effects
+    Returns:
+    a 1D numpy.ndarray containing the likelihood of obtaining the data,
+             x and n, for each probability in P, respectively
     """
-    if not isinstance(p, np.ndarray):
-        raise TypeError("p must be a numpy.ndarray")
-    if n <= 0:
-        raise ValueError("n must be a positive value")
-    if x < 0:
-        raise ValueError("x must be an integer that is greater than or "
-                         "equal to 0")
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+                "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if np.any(p < 0) or np.any(p > 1):
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if np.any(P < 0) or np.any(P > 1):
         raise ValueError("All values in P must be in the range [0, 1]")
+
     factorial_x = 1
     factorial_n_x = 1
     factorial_n = 1
@@ -37,5 +40,5 @@ def likelihood(x, n, p):
         factorial_n *= m
     coefficient = factorial_n / (factorial_x * factorial_n_x)
 
-    likelihoods = coefficient * (p ** x) * ((1 - p) ** (n - x))
+    likelihoods = coefficient * (P ** x) * ((1 - P) ** (n - x))
     return likelihoods
