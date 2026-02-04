4e1288a9dca70fcd4e6d1c03f4b9b072c71aad4e
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-02-04 13:28:33 +0000

    binom

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -11,22 +11,24 @@ class Binomial:
     e = 2.7182818285
 
     def __init__(self, data=None, n=1, p=0.5):
-        self.n = int(n)
-        self.p = float(p)
-
         if data is None:
-            if self.n <= 0:
+            if n <= 0:
                 raise ValueError("n must be a positive value")
-            if not (0 <= self.p <= 1):
-                raise ValueError("p must be in the range [0, 1]")
+            if not (0 < p < 1):
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
 
-            self.n = int(sum(data) / len(data))
-            self.p = float(sum(x / self.n for x in data) / len(data))
+            mean = sum(data) / len(data)
+            variance = sum((x - mean) ** 2 for x in data) / len(data)
+            p = 1 - variance / mean
+            self.n = int(round(mean / p))
+            self.p = float(mean / self.n)
 
     def pmf(self, k):
         """
