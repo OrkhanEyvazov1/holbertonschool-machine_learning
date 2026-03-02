51d2b571ab1075687eb29bed48274ba4a44f5a0d
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-03-02 11:52:48 +0000

    privite var

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""1. Neuron"""
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Constructor method
+        Args:
+            nx: number of input features to the neuron
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights vector"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
