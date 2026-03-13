1aed081a453002901cc9548ec4f9de73e00d9b7e
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-03-13 16:00:01 +0400

    feat: add save_model and load_model functions for Keras model management

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""9-model.py"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """Saves an entire model.
+
+    Args:
+        network: the model to save
+        filename: the path of the file that the model should be saved to
+    Returns:
+        None
+    """
+    network.save(filename)
+
+
+def load_model(filename):
+    """Loads an entire model.
+
+    Args:
+        filename: the path of the file that the model should be loaded from
+    Returns:
+        the loaded model
+    """
+    return K.models.load_model(filename)
