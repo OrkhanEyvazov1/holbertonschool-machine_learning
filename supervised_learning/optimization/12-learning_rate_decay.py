75f7f76ff76345a4cefeae2c6fdab8a5a4f0d904
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-03-17 14:57:32 +0400

    refactor: update learning rate decay function to remove global_step parameter and improve docstring

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -3,21 +3,20 @@
 import tensorflow as tf
 
 
-def create_learning_rate_decay_op(alpha, decay_rate, global_step, decay_step):
-    """Sets up learning rate decay in TensorFlow.
+def learning_rate_decay(alpha, decay_rate, decay_step):
+    """Creates a learning rate decay operation in TensorFlow using
+    inverse time decay in a stepwise fashion.
 
     Args:
         alpha: the original learning rate
-        decay_rate: the weight used to determine the rate of decay
-        global_step: the number of passes of gradient descent that have
-                     elapsed
+        decay_rate: the weight used to determine the rate at which alpha decays
         decay_step: the number of passes of gradient descent that should
                     occur before alpha is decayed further
     Returns:
-        A TensorFlow learning rate decay operation
+        The learning rate decay operation
     """
     return tf.keras.optimizers.schedules.InverseTimeDecay(
         initial_learning_rate=alpha,
         decay_steps=decay_step,
         decay_rate=decay_rate,
-        staircase=True)(global_step)
+        staircase=True)
