938aa412ce1631fde338d61350335723bd11cbb0
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-03-17 14:46:35 +0400

    refactor: replace TensorFlow learning rate decay with numpy implementation

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 """11-learning_rate_decay.py"""
-import tensorflow as tf
+import numpy as np
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
-    """Creates a learning rate decay operation in TensorFlow.
+    """Updates the learning rate using stepwise inverse time decay.
 
     Args:
         alpha: the original learning rate
         decay_rate: the weight used to determine the rate of decay
         global_step: the number of passes of gradient descent that have
-                     elapsed. Should be cast to an int.
+                     elapsed
         decay_step: the number of passes of gradient descent that should
-                    occur before alpha is decayed. Should be cast to an int.
+                    occur before alpha is decayed further
     Returns:
-        The learning rate decay operation
+        The updated learning rate
     """
-    return tf.compat.v1.train.inverse_time_decay(alpha,
-                                                  global_step,
-                                                  decay_step,
-                                                  decay_rate)
+    return alpha / (1 + decay_rate * np.floor(global_step / decay_step))
