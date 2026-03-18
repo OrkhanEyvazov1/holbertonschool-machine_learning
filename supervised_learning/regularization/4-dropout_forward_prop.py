90a4888bad63a25e9e82d71b3622719d2fba6387
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-03-18 15:28:41 +0400

    fix: correct formatting and improve dropout calculation in dropout_forward_prop function

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -4,22 +4,21 @@ import numpy as np
 
 
 def dropout_forward_prop(X, weights, L, keep_prob):
-    """
-    Conducts forward propagation using Dropout.
-    
+    """Conducts forward propagation using Dropout.
+
     Args:
         X: numpy.ndarray of shape (nx, m) containing input data
         weights: dictionary of weights and biases
         L: number of layers in the network
         keep_prob: probability that a node will be kept
-    
+
     Returns:
         dictionary containing outputs of each layer and dropout masks
     """
     cache = {}
     A = X
     cache['A0'] = A
-    
+
     for layer in range(1, L + 1):
         W = weights[f'W{layer}']
         b = weights[f'b{layer}']
@@ -29,8 +28,8 @@ def dropout_forward_prop(X, weights, L, keep_prob):
             A = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
         else:
             A = np.tanh(Z)
-            D = np.random.binomial(1, keep_prob, size=A.shape) / keep_prob
-            A = A * D
+            D = np.random.binomial(1, keep_prob, size=A.shape)
+            A = (A * D) / keep_prob
             cache[f'D{layer}'] = D
         cache[f'A{layer}'] = A
     return cache
