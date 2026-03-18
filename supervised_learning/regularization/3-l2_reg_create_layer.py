e9539a3c4ffdf994a1988d3cf061abd1b5b9f5f2
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-03-18 15:18:34 +0400

    feat: implement L2 regularized layer creation function in TensorFlow

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+"""L2 regularized layer creation in TensorFlow."""
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    Creates a TensorFlow layer with L2 regularization.
+
+    Args:
+        prev: tensor output from the previous layer
+        n: number of nodes in the new layer
+        activation: activation function for the new layer
+        lambtha: L2 regularization parameter
+
+    Returns:
+        Tensor output of the new layer
+    """
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode='fan_avg'
+    )
+    regularizer = tf.keras.regularizers.l2(lambtha)
+
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer,
+        kernel_regularizer=regularizer
+    )
+
+    return layer(prev)
