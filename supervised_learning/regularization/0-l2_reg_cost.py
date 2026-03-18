854e4e26022e56efc225a18b7032f6d5af53d228
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-03-18 14:34:58 +0400

    refactor: replace TensorFlow operations with numpy in L2 regularization cost function

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """0-l2_reg_cost.py"""
-import tensorflow as tf
-
+import numpy as np
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """Calculates the cost of a neural network with L2 regularization.
@@ -17,6 +16,6 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """
     l2_cost = 0
     for i in range(L):
-        l2_cost += tf.reduce_sum(tf.square(weights[i]))
+        l2_cost += np.sum(np.square(weights[i]))
     l2_cost *= lambtha / (2 * m)
     return cost + l2_cost
