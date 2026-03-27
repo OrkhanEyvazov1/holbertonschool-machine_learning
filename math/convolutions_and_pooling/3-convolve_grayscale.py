6ac0eeac8be5594bc61ef8659e4d22df573c9d50
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-03-27 21:39:25 +0400

    fix: enhance docstring clarity and correct padding logic in convolve_grayscale function

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""3-convolve_grayscale.py
-Contains the function convolve_grayscale
+"""
+3-convolve_grayscale.py
 """
 import numpy as np
 
@@ -8,60 +8,43 @@ import numpy as np
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     """
     Performs a convolution on grayscale images.
-
     Args:
-        images: numpy.ndarray with shape (m, h, w) containing multiple grayscale images
-                m is the number of images
-                h is the height in pixels of the images
-                w is the width in pixels of the images
-        kernel: numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
-                kh is the height of the kernel
-                kw is the width of the kernel
-        padding: Either a tuple of (ph, pw), 'same', or 'valid'
-                 'same': performs a same convolution
-                 'valid': performs a valid convolution
-                 tuple: ph is the padding for the height, pw is the padding for the width
-                 image is padded with 0's
-        stride: tuple of (sh, sw)
-                sh is the stride for the height of the image
-                sw is the stride for the width of the image
-
+        images (numpy.ndarray): Array of shape (m, h, w) containing grayscale images.
+        kernel (numpy.ndarray): Array of shape (kh, kw) containing the kernel for the convolution.
+        padding (str): Either 'same' or 'valid'.
+        stride (tuple): Stride for the convolution.
     Returns:
-        numpy.ndarray containing the convolved images
+        numpy.ndarray: Array of shape (m, oh, ow) containing the convolved images.
     """
-    m, h, w = images.shape
     kh, kw = kernel.shape
+    m, h, w = images.shape
     sh, sw = stride
 
     if padding == 'same':
-        ph = kh - 1
-        pw = kw - 1
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
     elif padding == 'valid':
-        ph = 0
-        pw = 0
+        ph = pw = 0
     else:
         ph, pw = padding
 
     padded_images = np.pad(
         images,
-        ((0, 0), (ph // 2, (ph + 1) // 2), (pw // 2, (pw + 1) // 2)),
-        mode='constant',
-        constant_values=0
-    )
+        ((0, 0), (ph, ph), (pw, pw)),
+        mode='constant')
 
-    padded_h = padded_images.shape[1]
-    padded_w = padded_images.shape[2]
-    out_h = (padded_h - kh) // sh + 1
-    out_w = (padded_w - kw) // sw + 1
-
-    output = np.zeros((m, out_h, out_w))
-
-    for i in range(out_h):
-        for j in range(out_w):
-            start_h = i * sh
-            start_w = j * sw
-
-            patch = padded_images[:, start_h:start_h + kh, start_w:start_w + kw]
-            output[:, i, j] = np.sum(patch * kernel, axis=(1, 2))
+    oh = (h + 2 * ph - kh) // sh + 1
+    ow = (w + 2 * pw - kw) // sw + 1
+    output = np.zeros((m, oh, ow))
+    for i in range(oh):
+        for j in range(ow):
+            h_start = i * sh
+            w_start = j * sw
+            h_end = h_start + kh
+            w_end = w_start + kw
+            output[:, i, j] = np.sum(
+                padded_images[:, h_start:h_end, w_start:w_end] * kernel,
+                axis=(1, 2)
+            )
 
     return output
