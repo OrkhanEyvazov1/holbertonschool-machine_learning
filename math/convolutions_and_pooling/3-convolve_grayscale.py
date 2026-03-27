29ed2ca82daf8316b9d614ec3e9d71e751c2889b
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-03-27 21:32:46 +0400

    fix: improve docstring clarity and formatting in convolve_grayscale function

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -6,27 +6,26 @@ import numpy as np
 
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
-    """performs a convolution on grayscale images with custom padding and stride
+    """
+    Performs a convolution on grayscale images.
 
     Args:
-        images: numpy.ndarray with shape (m, h, w) containing multiple
-                grayscale images
-            m: number of images
-            h: height in pixels of the images
-            w: width in pixels of the images
-        kernel: numpy.ndarray with shape (kh, kw) containing the kernel for
-                the convolution
-            kh: height of the kernel
-            kw: width of the kernel
-        padding: either a tuple of (ph, pw), 'same', or 'valid'
-            if 'same', performs a same convolution
-            if 'valid', performs a valid convolution
-            if a tuple:
-                ph is the padding for the height of the image
-                pw is the padding for the width of the image
-        stride: tuple of (sh, sw) containing the strides for the convolution
-            sh is the stride for the height of the image
-            sw is the stride for the width of the image
+        images: numpy.ndarray with shape (m, h, w) containing multiple grayscale images
+                m is the number of images
+                h is the height in pixels of the images
+                w is the width in pixels of the images
+        kernel: numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
+                kh is the height of the kernel
+                kw is the width of the kernel
+        padding: Either a tuple of (ph, pw), 'same', or 'valid'
+                 'same': performs a same convolution
+                 'valid': performs a valid convolution
+                 tuple: ph is the padding for the height, pw is the padding for the width
+                 image is padded with 0's
+        stride: tuple of (sh, sw)
+                sh is the stride for the height of the image
+                sw is the stride for the width of the image
+
     Returns:
         numpy.ndarray containing the convolved images
     """
@@ -35,23 +34,34 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     sh, sw = stride
 
     if padding == 'same':
-        pad_h = ((h - 1) * sh + kh - h) // 2
-        pad_w = ((w - 1) * sw + kw - w) // 2
+        ph = (kh - 1) // 2
+        pw = (kw - 1) // 2
     elif padding == 'valid':
-        pad_h, pad_w = 0, 0
+        ph = 0
+        pw = 0
     else:
-        pad_h, pad_w = padding
+        ph, pw = padding
 
-    padded_images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
-                           mode='constant')
+    padded_images = np.pad(
+        images,
+        ((0, 0), (ph, ph), (pw, pw)),
+        mode='constant',
+        constant_values=0
+    )
 
-    out_h = (h + 2 * pad_h - kh) // sh + 1
-    out_w = (w + 2 * pad_w - kw) // sw + 1
-    convolved_images = np.zeros((m, out_h, out_w))
+    padded_h = padded_images.shape[1]
+    padded_w = padded_images.shape[2]
+    out_h = (padded_h - kh) // sh + 1
+    out_w = (padded_w - kw) // sw + 1
+
+    output = np.zeros((m, out_h, out_w))
 
     for i in range(out_h):
         for j in range(out_w):
-            region = padded_images[:, i * sh:i * sh + kh, j * sw:j * sw + kw]
-            convolved_images[:, i, j] = np.sum(region * kernel, axis=(1, 2))
+            start_h = i * sh
+            start_w = j * sw
 
-    return convolved_images
+            patch = padded_images[:, start_h:start_h + kh, start_w:start_w + kw]
+            output[:, i, j] = np.sum(patch * kernel, axis=(1, 2))
+
+    return output
