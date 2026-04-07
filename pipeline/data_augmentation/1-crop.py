6638687b1b3cb71f713c413ce8bf0e977d19228f
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-04-07 13:32:46 +0400

    feat: update crop_image function to use random cropping method

--- pipeline/data_augmentation/1-crop.py
+++ pipeline/data_augmentation/1-crop.py
@@ -11,4 +11,4 @@ def crop_image(image, size):
     :param size: is a tuple containing the height and width of the crop
     :return: the cropped image
     """
-    return tf.image.resize_with_crop_or_pad(image, size[0], size[1])
+    return tf.image.random_crop(image, size)
