b59fc31f9fe9cdc2497c08ef3db2a258185e523c
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-04-07 13:18:03 +0400

    feat: add flip_image function for horizontal image flipping

---
+++ pipeline/data_augmentation/0-flip.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""0-flip.py
+"""
+import tensorflow as tf
+
+
+def flip_image(image):
+    """
+    a function that flips an image horizontally
+    :param image: is a 3D tf.Tensor containing the image to flip
+    :return: the flipped image
+    """
+    return tf.image.flip_left_right(image)
