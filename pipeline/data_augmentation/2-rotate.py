c22758e25d4860cb47b3f505cc4b698795d954b6
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-04-07 13:51:53 +0400

    feat: rename flip_image to rotate_image and update docstring for clarity

--- pipeline/data_augmentation/2-rotate.py
+++ pipeline/data_augmentation/2-rotate.py
@@ -4,10 +4,10 @@
 import tensorflow as tf
 
 
-def flip_image(image):
+def rotate_image(image):
     """
-    a function that flips an image horizontally
-    :param image: is a 3D tf.Tensor containing the image to flip
-    :return: the flipped image
+    a function that rotates an image by 90 degrees
+    :param image: is a 3D tf.Tensor containing the image to rotate
+    :return: the rotated image
     """
     return tf.image.rot90(image)
