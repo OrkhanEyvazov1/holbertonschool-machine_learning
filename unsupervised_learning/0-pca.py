50a6191aa2eeeca54c155cfbe5ca4104259ccc3b
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-05-12 22:52:04 +0400

    feat: add PCA implementation and corresponding README documentation

---
+++ unsupervised_learning/0-pca.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""0-pca.py
+"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """
+    Performs PCA on a dataset.
+    """
+    # Computes SVD
+    u, s, vh = np.linalg.svd(X)
+    
+    # Calculate the cumulative sum of the variance
+    cumulative_variance = np.cumsum(s) / np.sum(s)
+    
+    # Find the minimum number of dimensions needed to maintain `var`
+    nd = np.argwhere(cumulative_variance >= var)[0, 0] + 1
+    
+    # Return the weights matrix, W
+    W = vh[:nd].T
+    return W
