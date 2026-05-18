8f76ef7e2b3543d7f40143ccc4413f704c66c941
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-05-18 15:17:18 +0400

    refactor: remove unnecessary blank lines in initialize function

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -13,13 +13,10 @@ def initialize(X, k):
         return None, None, None
     if type(k) is not int or k <= 0 or k > X.shape[0]:
         return None, None, None
-    
     d = X.shape[1]
-    
     pi = np.full(shape=(k,), fill_value=1/k)
     m, _ = kmeans(X, k)
     if m is None:
         return None, None, None
     S = np.tile(np.identity(d), (k, 1, 1))
-    
     return pi, m, S
