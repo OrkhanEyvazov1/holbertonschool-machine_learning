96c3ee21c9a636d8b9c2344dff69f262391fa162
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-05-21 16:13:20 +0400

    feat: add k-means clustering implementation

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""10-kmeans.py"""
+import numpy as np
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    kmeans = sklearn.cluster.KMeans(n_clusters=k)
+    kmeans.fit(X)
+    C = kmeans.cluster_centers_
+    clss = kmeans.labels_
+    return C, clss
