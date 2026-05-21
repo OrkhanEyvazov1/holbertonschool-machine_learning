4dd60923b196db01c614153b1b7536e670abb471
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2026-05-21 16:14:38 +0400

    doc: update docstring for kmeans function to clarify arguments and return values

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -4,6 +4,14 @@ import sklearn.cluster
 
 
 def kmeans(X, k):
+    '''Performs K-means on a dataset.
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+        k: positive integer, number of clusters
+    Returns:
+        C, clss or None, None on failure
+        C: numpy.ndarray of shape (k, d) containing the centroid means for
+        each cluster'''
     kmeans = sklearn.cluster.KMeans(n_clusters=k)
     kmeans.fit(X)
     C = kmeans.cluster_centers_
